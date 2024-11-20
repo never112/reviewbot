ae1ec59ba28a064189da080bea6028ccc7a4d9c1
never112 <zhouxiaoliang@qiniu.com>
2024-11-20 14:42:42 +0800

    add p3c pmd check

---
+++ internal/linters/java/p3c-pmdcheck/p3cpmdcheck.go
@@ -0,0 +1,153 @@
+/*
+ Copyright 2024 Qiniu Cloud (qiniu.com).
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package pmdcheck
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/qiniu/reviewbot/internal/linters"
+	"github.com/qiniu/reviewbot/internal/lintersutil"
+	"github.com/qiniu/x/errors"
+	"github.com/qiniu/x/xlog"
+)
+
+// refer to https://pmd.github.io/
+const (
+	linterName = "pmdcheck"
+	pmdRuleURL = "https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources/category/java/bestpractices.xml"
+	pmdRuleDir = "/var/tmp/linters-config/"
+)
+
+func init() {
+	linters.RegisterPullRequestHandler(linterName, pmdCheckHandler)
+	linters.RegisterLinterLanguages(linterName, []string{".java"})
+}
+
+func pmdCheckHandler(ctx context.Context, a linters.Agent) error {
+	plog := lintersutil.FromContext(ctx)
+	var javaFiles []string
+	rulePath := a.LinterConfig.ConfigPath
+	for _, arg := range a.Provider.GetFiles(nil) {
+		if strings.HasSuffix(arg, ".java") {
+			javaFiles = append(javaFiles, arg)
+		}
+	}
+	if len(javaFiles) == 0 {
+		return nil
+	}
+	checkrulePath, checkerr := pmdRuleCheck(plog, rulePath, a)
+	if checkerr != nil {
+		plog.Errorf("p3c-pmd rule check failed: %v", checkerr)
+		return checkerr
+	}
+	a = argsApply(plog, a)
+	a.LinterConfig.Args = append(append(a.LinterConfig.Args, javaFiles...), "-R", checkrulePath)
+	return linters.GeneralHandler(ctx, plog, a, linters.ExecRun, pmdcheckParser)
+}
+
+func argsApply(log *xlog.Logger, a linters.Agent) linters.Agent {
+	config := a.LinterConfig
+	if len(config.Command) == 1 && config.Command[0] == linterName {
+		config.Command = []string{"java"}
+	}
+	//  java -cp java net.sourceforge.pmd.PMD  -d D:\CodeProject\demo2  -R D:\CodeProject\p3c\p3c-pmd\src\main\resources\rulesets\java\ali-comment.xml,D:\CodeProject\p3c\p3c-pmd\src\main\resources\rulesets\java\ali-oop.xml
+	//java -cp p3c-pmd-2.1.1-jar-with-dependencies.jar net.sourceforge.pmd.PMD -d /Users/huey/dev/java/ideaWorkSpace/rongc/zx-bme/staff-zx-service/staff-zx-pfmc/src/main/java/com/sunacwy/staff/zx/pfmc -R rulesets/java/ali-comment.xml
+	log.Info("p3c-pmdcheck comamnd:" + strings.Join(config.Command, " "))
+	if linters.IsEmpty(config.Args...) {
+		args := append([]string{}, "-cp")
+		args = append(args, "p3c-pmd-2.1.1-jar-with-dependencies.jar", "net.sourceforge.pmd.PMD")
+		args = append(args, "-R", "net.sourceforge.pmd.PMD", "rulesets/java/ali-comment.xml")
+		config.Args = args
+	}
+	a.LinterConfig = config
+	return a
+}
+
+func pmdcheckParser(plog *xlog.Logger, output []byte) (map[string][]linters.LinterOutput, []string) {
+	lineParse := func(line string) (*linters.LinterOutput, error) {
+		// pmdcheck will output lines starting with ' [WARN]' or '[ERROR]'  warring/error information
+		// which are no meaningful for the reviewbot scenario, so we discard them
+		if strings.Contains(line, "[WARN]") || strings.Contains(line, "[ERROR]") {
+			return nil, nil
+		}
+		return linters.GeneralLineParser(strings.TrimLeft(line, " "))
+	}
+	return linters.Parse(plog, output, lineParse)
+}
+
+func getFileFromURL(plog *xlog.Logger, url string) (string, error) {
+	newfile := filepath.Join(pmdRuleDir, filepath.Base(url))
+	res, err := http.Get(url)
+	if err != nil {
+		plog.Errorf("the file download encountered an error, Please check the file download url: %v, the error is:%v", url, err)
+		return "", err
+	}
+	if err := os.MkdirAll(pmdRuleDir, os.ModePerm); err != nil {
+		plog.Fatalf("failed to  create check rule config dir: %v", err)
+	}
+	f, err := os.Create(newfile)
+	if err != nil {
+		plog.Errorf("the file saving encountered an error, Please check the directory: %v", err)
+		return "", err
+	}
+	_, err = io.Copy(f, res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		plog.Errorf("the file saving   encountered an error: %v", err)
+		return "", err
+	}
+	return newfile, nil
+}
+
+func pmdRuleCheck(plog *xlog.Logger, pmdConf string, a linters.Agent) (string, error) {
+	tmpnewfile := filepath.Join(pmdRuleDir, filepath.Base(pmdRuleURL))
+	if pmdConf == "" {
+		absfilepath, _ := lintersutil.FileExists(tmpnewfile)
+		if absfilepath != "" {
+			return absfilepath, nil
+		}
+		downloadfilepath, err := getFileFromURL(plog, pmdRuleURL)
+		if err != nil {
+			plog.Errorf("the pmd rule file download faild: %v", err)
+			return "", err
+		}
+		return downloadfilepath, nil
+	}
+	if strings.HasPrefix(pmdConf, "http://") || strings.HasPrefix(pmdConf, "https://") {
+		downloadfilepath, err := getFileFromURL(plog, pmdRuleURL)
+		if err != nil {
+			plog.Errorf("the pmd rule file download faild: %v", err)
+			return "", err
+		}
+		return downloadfilepath, nil
+	}
+	absfilepath, exist := lintersutil.FileExists(pmdConf)
+	if exist {
+		return absfilepath, nil
+	}
+	pmdconfpath := filepath.Join(a.LinterConfig.WorkDir, pmdConf)
+	abspmdfilepath, _ := lintersutil.FileExists(pmdconfpath)
+	if absfilepath != "" {
+		return abspmdfilepath, nil
+	}
+	return "", errors.New("the pmd rule file not exist")
+}
---
+++ internal/linters/java/p3c-pmdcheck/p3cpmdcheck_test.go
@@ -0,0 +1,174 @@
+/*
+ Copyright 2024 Qiniu Cloud (qiniu.com).
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package pmdcheck
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/qiniu/reviewbot/config"
+	"github.com/qiniu/reviewbot/internal/linters"
+	"github.com/qiniu/x/errors"
+	"github.com/qiniu/x/log"
+	"github.com/qiniu/x/xlog"
+)
+
+func TestArgs(t *testing.T) {
+	tp := true
+	tcs := []struct {
+		id    string
+		input linters.Agent
+		want  linters.Agent
+	}{
+		{
+			id: "case1 - default command and args",
+			input: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"pmdcheck"},
+				},
+			},
+			want: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"pmd"},
+					Args:    []string{"check", "-f", "emacs"},
+				},
+			},
+		},
+		{
+			id: "case2 - custom command",
+			input: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"/usr/pmdcheck"},
+				},
+			},
+			want: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"/usr/pmdcheck"},
+					Args:    []string{"check", "-f", "emacs"},
+				},
+			},
+		},
+		{
+			id: "case3 - custom args",
+			input: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"pmdcheck"},
+					Args:    []string{"check", "-f", "xml"},
+				},
+			},
+			want: linters.Agent{
+				LinterConfig: config.Linter{
+					Enable:  &tp,
+					Command: []string{"pmd"},
+					Args:    []string{"check", "-f", "xml"},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.id, func(t *testing.T) {
+			got := argsApply(xlog.New("ut"), tc.input)
+			if !reflect.DeepEqual(got.LinterConfig, tc.want.LinterConfig) {
+				t.Errorf("args() = %v, want %v", got.LinterConfig, tc.want.LinterConfig)
+			}
+		})
+	}
+}
+
+func TestPmdRuleCheck(t *testing.T) {
+	dir := "/var/tmp/linters-config/"
+	a := linters.Agent{}
+	a.LinterConfig.WorkDir = ""
+	tc := []struct {
+		input    string
+		expected string
+		err      error
+	}{
+		{
+			input:    "",
+			expected: dir + "bestpractices.xml",
+			err:      nil,
+		},
+		{
+			input:    "/config/linters-config/.notjava-bestpractices.xml",
+			expected: "",
+			err:      errors.New("the pmd rule file not exist"),
+		},
+		{
+			input:    "https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources/category/java/bestpractices.xml",
+			expected: dir + "bestpractices.xml",
+			err:      nil,
+		},
+	}
+	for _, c := range tc {
+		got, err := pmdRuleCheck(xlog.New("ut"), c.input, a)
+		log.Info("E:" + c.input)
+		if !reflect.DeepEqual(got, c.expected) {
+			t.Errorf("pmdcheckParser(): %v, expected: %v", got, c.expected)
+		}
+		if !reflect.DeepEqual(err, c.err) {
+			t.Errorf("pmdcheckParser() error: %v, unexpected: %v", err, c.err)
+			return
+		}
+	}
+	t.Cleanup(func() {
+		_ = os.RemoveAll(dir)
+	})
+}
+
+func TestFormatPmdCheckLine(t *testing.T) {
+	tc := []struct {
+		input      []byte
+		expected   map[string][]linters.LinterOutput
+		unexpected []string
+	}{
+		{
+			input: []byte(`[ERROR] No such file ./test3.java
+[WARN] Progressbar rendering conflicts with reporting to STDOUT. No progressbar will be shown. Try running with argument -r <file> to output the report to a file instead.
+[WARN] This analysis could be faster, please consider using Incremental Analysis: https://docs.pmd-code.org/pmd-doc-7.4.0/pmd_userdocs_incremental_analysis.html
+./test.java:8: Avoid unused local variables such as 'test'.`),
+			expected: map[string][]linters.LinterOutput{
+				"./test.java": {
+					{
+						File:    "./test.java",
+						Line:    8,
+						Column:  0,
+						Message: "Avoid unused local variables such as 'test'.",
+					},
+				},
+			},
+			unexpected: nil,
+		},
+	}
+	for _, c := range tc {
+		got, err := pmdcheckParser(xlog.New("UnitJavaPmdCheckTest"), c.input)
+		if !reflect.DeepEqual(err, c.unexpected) {
+			t.Errorf("stylecheckParser() error: %v, unexpected: %v", err, c.unexpected)
+			return
+		}
+		if !reflect.DeepEqual(got, c.expected) {
+			t.Errorf("stylecheckParser(): %v, expected: %v", got, c.expected)
+		}
+	}
+}
